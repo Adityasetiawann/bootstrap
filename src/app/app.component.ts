4575069046a7babe68424d145a6a7ad5f4517894
Adityasetiawann <kodityasetiawan@gmail.com>
2022-12-16 10:21:02 +0700

    bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bootstrap';
+  
+  nama = "Aditya Setiawan";
+  nim = "172101880";
+  title = 'angular';
 }
